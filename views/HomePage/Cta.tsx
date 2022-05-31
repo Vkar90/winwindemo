9f4919291a6a6bb1bc5fd5423226986fa711c357
Vkar90 <kardamitsis.e@gmail.com>
2022-05-31 16:32:15 +0300

    add 2nd podcast episode and changed description

--- views/HomePage/Cta.tsx
+++ views/HomePage/Cta.tsx
@@ -16,9 +16,12 @@ export default function Cta() {
           <OverTitle>corporate social responsibility</OverTitle>
           <SectionTitle>Want to know how to get in a Win-Win situation?</SectionTitle>
           <Description>
-          We help you dominate the future and achieve a triple bottom line: Profit, People, Planet. We are committed to doing good. We make sure that all causes are making a social impact with regular follow-ups.
+          {/* We help you dominate the future and achieve a triple bottom line: Profit, People, Planet. We are committed to doing good. We make sure that all causes are making a social impact with regular follow-ups.<br></br><br></br> */}
+          Corporate Social Responsibility decoded and businesses “doing good” analyzed. Entrepreneurship and social needs experts share their knowledge on modern CSR with the WinWin team. Should businesses take a stand on current social and environmental issues? Can CSR have a real impact? How will social entities make their message stronger? In what ways can businesses dialogue with consumers become more meaningful and beneficial? 
           </Description>
+          <OverTitle>Listen to our podcast here: </OverTitle>
           <iframe style={{borderRadius:"12px"}} title="CSR podcast on Athen's Voice" src="https://open.spotify.com/embed/episode/2M1b8s5cy5EP9JSj2G2GAL?utm_source=generator&t=0" width={"100%"} height={232} frameBorder="0" allowFullScreen allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+          <iframe style={{borderRadius:"12px"}} title="CSR podcast on Athen's Voice" src="https://open.spotify.com/embed/episode/5LRXBbVZ9LDkqXiv8f2WgZ?utm_source=generator&t=0" width={"100%"} height={232} frameBorder="0" allowFullScreen allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>   
           <ButtonGroup>
             <NextLink href="/contact" passHref>
               <Button>
